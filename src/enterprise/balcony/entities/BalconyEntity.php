533d473778c56f745edc5fd93bde4e8551976e18
felizardochirindja <felizardo.chirindja@gmail.com>
2022-09-06 10:58:19 +0200

    created balcony entity class

---
+++ src/enterprise/balcony/entities/BalconyEntity.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Seb\Enterprise\Balcony\Entities;
+
+use DomainException;
+
+final class BalconyEntity
+{
+    private string $attendantName;
+    private int $number;
+ 
+    public function getName(): string
+    {
+        return "balcão " . $this->number;
+    }
+ 
+    public function getAttendantName(): string
+    {
+        return $this->attendantName;
+    }
+ 
+    public function setAttendantName($attendantName): self
+    {
+        $this->attendantName = $attendantName;
+        return $this;
+    }
+
+    public function getNumber(): int
+    {
+        return $this->number;
+    }
+
+    public function setNumber($number): self
+    {
+        if ($number < 1) {
+            throw new DomainException("balcony number must be greeter than 0", 1);
+        }
+
+        $this->number = $number;
+        return $this;
+    }
+}
