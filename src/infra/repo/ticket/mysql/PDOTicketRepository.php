61588c2ea864fc4b7c652729bf7c8247b061d90e
felizardochirindja <felizardo.chirindja@gmail.com>
2022-09-09 12:20:03 +0200

    created ticket repository that uses PDO object and implements create ticket interface

---
+++ src/infra/repo/ticket/mysql/PDOTicketRepository.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Seb\Infra\Repo\Ticket\MySQL;
+
+use DateTimeInterface as DateTime;
+use PDO;
+use Seb\Infra\Repo\Interfaces\PDORepository;
+use Seb\Infra\Repo\Ticket\Interfaces\CreateTicketRepository;
+
+final class PDOTicketRepository extends PDORepository implements CreateTicketRepository
+{
+    public function createTicket(DateTime $emitionMoment, int $code, string $status): array
+    {
+        $query = '
+            insert into tickets set
+            code = :code,
+            emition_moment = :emition_moment,
+            status = :status;
+        ';
+
+        $formatetEmitionMoment = $emitionMoment->format('Y-m-d H:i:s');
+
+        $statment = $this->connection->prepare($query);
+        $statment->bindParam(':code', $code);
+        $statment->bindParam(':emition_moment', $formatetEmitionMoment);
+        $statment->bindParam(':status', $status);
+        $statment->execute();
+
+        $insertedTicketId = $this->getLastTicketInsertdId();
+        $ticket = $this->readTicketById($insertedTicketId);
+        return $ticket;
+    }
+
+    private function getLastTicketInsertdId(): int
+    {
+        $ticketId = $this->connection->lastInsertId();
+        return $ticketId;
+    }
+
+    public function readTicketById(int $id): array
+    {
+        $query = '
+            select * from tickets where id = :id;
+        ';
+
+        $statment = $this->connection->prepare($query);
+        $statment->bindParam(':id', $id);
+        $statment->execute();
+
+        $ticket = $statment->fetch(PDO::FETCH_ASSOC);
+        return $ticket;
+    }
+}
