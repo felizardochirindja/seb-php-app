d997b8186300ca494e32be1c2a2a31e12833e142
felizardochirindja <felizardo.chirindja@gmail.com>
2022-09-16 10:45:16 +0200

    unsetting unecessary properties from emit ticket output

--- src/app/ticket/emitTicket/dto/EmitTicketOutput.php
+++ src/app/ticket/emitTicket/dto/EmitTicketOutput.php
@@ -5,9 +5,7 @@ namespace Seb\App\Ticket\EmitTicket\DTO;
 final class EmitTicketOutput
 {
     public function __construct(
-        public string $emitionMoment,
-        public int $code,
-        public string $companyName,
-        public string $messageForClient,
+        public int $ticketCode,
+        public string $pdfCode,
     ) {}
 }
