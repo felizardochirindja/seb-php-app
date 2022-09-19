f46daa70f99f2c73a2cc25efb378de2eed719742
felizardochirindja <felizardo.chirindja@gmail.com>
2022-09-19 11:28:19 +0200

    Update EmitTicketUseCase.php
    generated new code for the inserted tickets

--- src/app/ticket/emitTicket/EmitTicketUseCase.php
+++ src/app/ticket/emitTicket/EmitTicketUseCase.php
@@ -2,21 +2,27 @@
 
 namespace Seb\App\Ticket\EmitTicket;
 
+use DateTime;
 use Seb\Infra\Adapters\Ticket\PDFGenerator\TicketPDFGeneratable as TicketPDFGenerator;
 use Seb\App\Ticket\EmitTicket\DTO\EmitTicketOutput;
 use Seb\Enterprise\Ticket\Entities\TicketEntity;
-use Seb\Infra\Repo\Ticket\Interfaces\CreateTicketRepository;
+use Seb\Infra\Repo\Ticket\Interfaces\TicketRepository;
 
 final class EmitTicketUseCase
 {
     public function __construct(
-        private CreateTicketRepository $repository,
+        private TicketRepository $repository,
         private TicketEntity $ticket,
         private TicketPDFGenerator $pdfGenerator,
     ) {}
 
     public function execute(): EmitTicketOutput
     {
+        $ticketCode = (int) $this->repository->readLastInsertedTicket()['code'];
+        $this->ticket->setCode(++$ticketCode)
+            ->setStatus('pending')
+            ->setEmitionMoment(new DateTime());
+
         $ticket = $this->repository->createTicket(
             $this->ticket->getEmitionMoment(),
             $this->ticket->getCode(),
