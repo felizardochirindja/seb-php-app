0eba8e47d19a248a6ec51b8c8a79d8d7384804a1
felizardochirindja <felizardo.chirindja@gmail.com>
2022-09-25 13:41:47 +0200

    Create MarkTicketAsServedUseCase.php

---
+++ src/app/balcony/markTicketAsServed/MarkTicketAsServedUseCase.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Seb\App\Balcony\MarkTicketAsServed;
+
+use DateTime;
+use Seb\Enterprise\Balcony\Entities\BalconyEntity;
+use Seb\Enterprise\Ticket\Entities\TicketEntity;
+use Seb\Infra\Repo\Balcony\Interfaces\BalconyRepository;
+use Seb\Infra\Repo\Ticket\Interfaces\TicketRepository;
+
+final class MarkTicketAsServedUseCase
+{
+    public function __construct(
+        private TicketRepository $ticketRepo,
+        private BalconyRepository $balconyRepo,
+        private BalconyEntity $balcony,
+        private TicketEntity $ticket,
+    ) {}
+
+    public function execute(int $balconyNumber): bool
+    {
+        $balconyStatus = $this->balcony->setStatus('not in service')->getStatus();
+        $this->balconyRepo->updateBalconyStatus($balconyNumber, $balconyStatus);
+
+        $ticketStatus = $this->ticket->setStatus('in service')->getStatus();
+        $ticketId = $this->ticketRepo->readServiceByBalconyNumberAndStatus($balconyNumber, $ticketStatus)['id'];
+
+        $this->balconyRepo->setEndServiceMoment($ticketId, $balconyNumber, new DateTime());
+
+        $ticketStatus = $this->ticket->setStatus('attended')->getStatus();
+        $this->ticketRepo->updateTicketStatus($ticketId, $ticketStatus);
+
+        return true;
+    }
+}
