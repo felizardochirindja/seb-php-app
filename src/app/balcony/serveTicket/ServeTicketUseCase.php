879bf1f8f8eafefd2418fb2258df5056a4d57d1e
felizardochirindja <felizardo.chirindja@gmail.com>
2022-09-26 23:00:18 +0200

    balcony just serve new ticket if is not in service

--- src/app/balcony/serveTicket/ServeTicketUseCase.php
+++ src/app/balcony/serveTicket/ServeTicketUseCase.php
@@ -3,6 +3,7 @@
 namespace Seb\App\Balcony\ServeTicket;
 
 use DateTimeImmutable;
+use Exception;
 use Seb\Enterprise\Balcony\Entities\BalconyEntity;
 use Seb\Enterprise\Ticket\Entities\TicketEntity;
 use Seb\Infra\Repo\Balcony\Interfaces\BalconyRepository;
@@ -19,6 +20,11 @@ final class ServeTicketUseCase
 
     public function execute(int $balconyNumber): bool
     {
+        $actualBalconyStatus = $this->balconyRepo->readBalconyStatus($balconyNumber);
+
+        if ($actualBalconyStatus === 'in service') throw new Exception('balcony ' . $balconyNumber . ' is already in service');
+        if ($actualBalconyStatus === 'inactive') throw new Exception('inactive ' . $balconyNumber . ' is not active');
+
         $firstPendingTicket = $this->ticketRepo->readFirstTicketByStatus('pending');
 
         $startMoment = new DateTimeImmutable();
