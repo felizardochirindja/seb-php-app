9d2b8c3f329dbd9c4c956105866da2612b34c072
felizardochirindja <felizardo.chirindja@gmail.com>
2022-10-16 11:17:19 +0200

    namespaces to request and response interfaces

--- src/platform/web/ticket/routes/ticketRoutesHandler.php
+++ src/platform/web/ticket/routes/ticketRoutesHandler.php
@@ -1,8 +1,8 @@
 <?php
 
 use Dompdf\Dompdf;
-use Psr\Http\Message\RequestInterface;
-use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\RequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
 use Seb\Adapters\DB\PDO\MySQL\MySQLPDOConnector;
 use Seb\Adapters\Libs\Ticket\PDFGenerator\DomPDF\DomPDFTicketGeneratorAdapter;
 use Seb\Adapters\Repo\Balcony\MySQL\PDOBalconyRepository;
@@ -12,7 +12,7 @@ use Seb\Platform\Web\Ticket\Controller\TicketController;
 use Slim\App;
 
 return function (App $app) {
-    $app->get('/emit_ticket', function (RequestInterface $request, ResponseInterface $response, $args) {
+    $app->get('/emit_ticket', function (Request $request, Response $response, $args) {
         $mysqlConnector = new MySQLPDOConnector('mysql:host=localhost;dbname=seb', 'root', '');
     
         $domPDF = new Dompdf();
