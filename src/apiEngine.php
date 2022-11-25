39d6119649522e45f546d92202817173f5de903b
felizardochirindja <felizardo.chirindja@gmail.com>
2022-11-25 12:46:07 +0200

    imported balcony routes on app engine file

--- src/apiEngine.php
+++ src/apiEngine.php
@@ -2,6 +2,9 @@
 
 use DI\Container;
 use Slim\Factory\AppFactory;
+use Fig\Http\Message\StatusCodeInterface;
+use Psr\Http\Message\RequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response; 
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -17,6 +20,9 @@ $middleware = require_once __DIR__ . '/config/middleware.php';
 $middleware($app);
 
 $ticketRoutes = require_once __DIR__ . '/platform/web/ticket/routes/ticketRoutesHandler.php';
+$balconyRoutes = require_once __DIR__ . '/platform/web/balcony/routes/balconyRoutesHandler.php';
+
 $ticketRoutes($app);
+$balconyRoutes($app);
 
 $app->run();
