943e4f2c887fb4ab896d0014fdd02c372aa7d7ae
felizardochirindja <felizardo.chirindja@gmail.com>
2023-02-03 03:25:09 +0200

    removed a container to handle not allowed methods

--- src/config/settings.php
+++ src/config/settings.php
@@ -3,8 +3,7 @@
 declare(strict_types=1);
 
 use DI\Container;
-use Psr\Http\Message\RequestInterface as Request;
-use Psr\Http\Message\ResponseInterface as Response;
+use Monolog\Logger;
 
 return function (Container $container) {
     $container->set('settings', function() {
@@ -13,25 +12,12 @@ return function (Container $container) {
             'displayErrorDetails' => true, // Should be set to false in production
             'logErrors' => true,
             'logErrorDetails' => true,
-            // 'logger' => [
-            //     'name' => 'slim-app',
-            //     'path' => __DIR__ . '/../logs/app.log',
-            //     'level' => Logger::DEBUG,
-            // ],
+            
+            'logger' => [
+                'name' => 'seb',
+                'path' => __DIR__ . '/../../logs/app.log',
+                'level' => Logger::DEBUG,
+            ],
         ];
-    });
-
-    $container->set('', function() {
-        return function (Request $request, Response $response, array $methods) {
-            return $response->withStatus(405)
-                ->withHeader('Allow', implode(', ', $methods))
-                ->withHeader('Content-type', 'aplication/json')
-                ->getBody()
-                ->write([
-                    "status" => false,
-                    "message" => "Method Not Allowed",
-                    'tip' => 'Alowed HTTP methods: ' . implode(', ', $methods),
-                ]);
-        };
     });
 };
