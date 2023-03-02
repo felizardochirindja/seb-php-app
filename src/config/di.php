10d51f788dab816d310a96fe3ef5083ba6509020
felizardochirindja <felizardo.chirindja@gmail.com>
2023-03-03 00:55:57 +0200

    refactored

--- src/config/di.php
+++ src/config/di.php
@@ -9,28 +9,29 @@ use Monolog\Processor\UidProcessor;
 use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
 use Seb\Adapters\DB\PDO\MySQL\MySQLPDOConnector;
-use Seb\Adapters\DB\PDO\PDOConnector;
+use Seb\Adapters\DB\PDO\PDOConnectable as PDOConnector;
 use Seb\Adapters\Libs\Ticket\PDFGenerator\DomPDF\DomPDFTicketGeneratorAdapter;
 use Seb\Adapters\Libs\Ticket\PDFGenerator\TicketPDFGeneratable as TicketPDFGenerator;
 use Seb\Adapters\Repo\Balcony\Interfaces\BalconyRepository;
 use Seb\Adapters\Repo\Balcony\MySQL\PDOBalconyRepository;
 use Seb\Adapters\Repo\Ticket\Interfaces\TicketRepository;
 use Seb\Adapters\Repo\Ticket\MySQL\PDOTicketRepository;
+use Seb\App\UseCases\Ticket\EmitTicket\EmitTicketUseCase;
 
 return function (ContainerBuilder $containerBuilder) {
     $containerBuilder->addDefinitions([
-        PDO::class => DI\create(MySQLPDOConnector::class)->constructor('mysql:host=localhost;dbname=seb', 'root', ''),
-        TicketPDFGenerator::class => DI\create(DomPDFTicketGeneratorAdapter::class)->constructor(Dompdf::class),
+        TicketPDFGenerator::class => DI\create(DomPDFTicketGeneratorAdapter::class)->constructor(new Dompdf()),
+        PDOConnector::class => DI\create(MySQLPDOConnector::class)->constructor('mysql:host=localhost;dbname=seb', 'root', ''),
         TicketRepository::class => function(ContainerInterface $container) {
-            return new PDOTicketRepository($container->get(MySQLPDOConnector::class)->getConnection());
+            return new PDOTicketRepository($container->get(PDOConnector::class)->getConnection());
         },
         BalconyRepository::class => function(ContainerInterface $container) {
-            return new PDOBalconyRepository($container->get(MySQLPDOConnector::class)->getConnection());
+            return new PDOBalconyRepository($container->get(PDOConnector::class)->getConnection());
         },
         LoggerInterface::class => function (ContainerInterface $container) {
             $settings = $container->get('settings');
             
-            $loggerSettings = $settings->get('logger');
+            $loggerSettings = $settings['logger'];
             $logger = new Logger($loggerSettings['name']);
 
             $processor = new UidProcessor();
