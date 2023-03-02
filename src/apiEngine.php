bf2081de8b0c0220ccaba3b61bb068f5ef0133ac
felizardochirindja <felizardo.chirindja@gmail.com>
2023-03-02 16:10:37 +0200

    changed container to ContainterBuilder

--- src/apiEngine.php
+++ src/apiEngine.php
@@ -1,14 +1,19 @@
 <?php
 
-use DI\Container;
+use DI\ContainerBuilder;
 use Slim\Factory\AppFactory;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$container = new Container();
+$containerBuilder = new ContainerBuilder();
 
 $settings = require_once __DIR__ . '/config/settings.php';
-$settings($container);
+$settings($containerBuilder);
+
+$dependencies = require_once __DIR__ . '/config/di.php';
+$dependencies($containerBuilder);
+
+$container = $containerBuilder->build();
 
 AppFactory::setContainer($container);
 $app = AppFactory::create();
--- src/config/settings.php
+++ src/config/settings.php
@@ -2,22 +2,24 @@
 
 declare(strict_types=1);
 
-use DI\Container;
+use DI\ContainerBuilder;
 use Monolog\Logger;
 
-return function (Container $container) {
-    $container->set('settings', function() {
-        return [
-            'name' => 'example slim application',
-            'displayErrorDetails' => true, // Should be set to false in production
-            'logErrors' => true,
-            'logErrorDetails' => true,
-            
-            'logger' => [
-                'name' => 'seb',
-                'path' => __DIR__ . '/../../logs/app.log',
-                'level' => Logger::DEBUG,
-            ],
-        ];
-    });
+return function (ContainerBuilder $container) {
+    $container->addDefinitions([
+        'settings' => function() {
+            return [
+                'name' => 'example slim application',
+                'displayErrorDetails' => true, // false in production
+                'logErrors' => true,
+                'logErrorDetails' => true,
+                
+                'logger' => [
+                    'name' => 'seb',
+                    'path' => __DIR__ . '/../../logs/app.log',
+                    'level' => Logger::DEBUG,
+                ],
+            ];
+        }
+    ]);
 };
