7145f5620a5ce9a32a66b5cb408f691a34211145
felizardochirindja <felizardo.chirindja@gmail.com>
2023-03-03 00:59:39 +0200

    clean ticket routes by using dependency injection

--- src/platform/web/ticket/routes/ticketRoutesHandler.php
+++ src/platform/web/ticket/routes/ticketRoutesHandler.php
@@ -1,45 +1,17 @@
 <?php
 
-use Dompdf\Dompdf;
-use Monolog\Handler\StreamHandler;
-use Monolog\Logger;
-use Monolog\Processor\UidProcessor;
 use Psr\Http\Message\RequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
-use Seb\Adapters\DB\PDO\MySQL\MySQLPDOConnector;
-use Seb\Adapters\Libs\Ticket\PDFGenerator\DomPDF\DomPDFTicketGeneratorAdapter;
-use Seb\Adapters\Repo\Balcony\MySQL\PDOBalconyRepository;
-use Seb\Adapters\Repo\Ticket\MySQL\PDOTicketRepository;
 use Seb\App\UseCases\Ticket\EmitTicket\EmitTicketUseCase;
 use Seb\Platform\Web\Ticket\Controller\TicketController;
 use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
 return function (App $app) {
-    $app->group('/ticket', function(Group $group) {
-        $group->get('/emit_ticket', function (Request $request, Response $response) {
-            $mysqlConnector = new MySQLPDOConnector('mysql:host=localhost;dbname=seb', 'root', '');
-    
-            $domPDF = new Dompdf();
-            $ticketPdfGen = new DomPDFTicketGeneratorAdapter($domPDF);
-            $createTicketRepo = new PDOTicketRepository($mysqlConnector->getConnection());
-            $balconyRepo = new PDOBalconyRepository($mysqlConnector->getConnection());
-
-            $logger = new Logger('Seb');
-
-            $processor = new UidProcessor();
-            $logger->pushProcessor($processor);
-
-            $handler = new StreamHandler(__DIR__ . '/../../../../../logs/app.log', Logger::DEBUG);
-            $logger->pushHandler($handler);
-    
-            $emitTicketUseCase = new EmitTicketUseCase(
-                $createTicketRepo,
-                $balconyRepo,
-                $ticketPdfGen,
-                $logger
-            );
-    
+    $app->group('/ticket', function(Group $group) use ($app) {
+        $group->get('/emit_ticket', function (Request $request, Response $response) use ($app) {
+            /** @var EmitTicketUseCase $emitTicketUseCase */
+            $emitTicketUseCase = $app->getContainer()->get(EmitTicketUseCase::class);
             $controller = new TicketController($request, $response);
             $response = $controller->emitTicket($emitTicketUseCase);
     
